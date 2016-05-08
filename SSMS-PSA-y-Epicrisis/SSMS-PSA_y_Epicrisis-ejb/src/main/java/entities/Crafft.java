0ad59038896698f95db43d87d5660e621e2c9f16
seba1165 <sebastian.calderon@usach.cl>
2016-05-08 18:05:56 -0300

    Create en Crafft con condiciones

---
+++ SSMS-PSA-y-Epicrisis/SSMS-PSA_y_Epicrisis-ejb/src/main/java/entities/Crafft.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Seba
+ */
+@Entity
+public class Crafft implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_CRAFFT")
+    private Long id;
+    
+    @NotNull(message="Debe ingresar una respuesta")
+    @Column(name="A1")
+    private boolean A1;
+    
+    @NotNull(message="Debe ingresar una respuesta")
+    @Column(name="A2")
+    private boolean A2;
+    
+    @NotNull(message="Debe ingresar una respuesta")
+    @Column(name="A3")
+    private boolean A3;
+
+    @Column(name="B1")
+    private boolean B1;
+    
+    @Column(name="B2")
+    private boolean B2;
+    
+    @Column(name="B3")
+    private boolean B3;
+    
+    @Column(name="B4")
+    private boolean B4;
+    
+    @Column(name="B5")
+    private boolean B5;
+    
+    @Column(name="B6")
+    private boolean B6;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isA1() {
+        return A1;
+    }
+
+    public void setA1(boolean A1) {
+        this.A1 = A1;
+    }
+
+    public boolean isA2() {
+        return A2;
+    }
+
+    public void setA2(boolean A2) {
+        this.A2 = A2;
+    }
+
+    public boolean isA3() {
+        return A3;
+    }
+
+    public void setA3(boolean A3) {
+        this.A3 = A3;
+    }
+
+    public boolean isB1() {
+        return B1;
+    }
+
+    public void setB1(boolean B1) {
+        this.B1 = B1;
+    }
+
+    public boolean isB2() {
+        return B2;
+    }
+
+    public void setB2(boolean B2) {
+        this.B2 = B2;
+    }
+
+    public boolean isB3() {
+        return B3;
+    }
+
+    public void setB3(boolean B3) {
+        this.B3 = B3;
+    }
+
+    public boolean isB4() {
+        return B4;
+    }
+
+    public void setB4(boolean B4) {
+        this.B4 = B4;
+    }
+
+    public boolean isB5() {
+        return B5;
+    }
+
+    public void setB5(boolean B5) {
+        this.B5 = B5;
+    }
+
+    public boolean isB6() {
+        return B6;
+    }
+
+    public void setB6(boolean B6) {
+        this.B6 = B6;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Crafft)) {
+            return false;
+        }
+        Crafft other = (Crafft) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.CRAFFT[ id=" + id + " ]";
+    }
+    
+}
--- SSMS-PSA-y-Epicrisis/SSMS-PSA_y_Epicrisis-ejb/src/test/java/entities/UsuarioTest.java
+++ SSMS-PSA-y-Epicrisis/SSMS-PSA_y_Epicrisis-ejb/src/test/java/entities/UsuarioTest.java
@@ -11,7 +11,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -33,89 +32,11 @@ public class UsuarioTest {
     @Before
     public void setUp() {
         usuario = new Usuario();
-        usuario.setCorreo("correo");
-        usuario.setId(Long.MIN_VALUE);
-        usuario.setNombre("nombre");
         usuario.setPassword("");
-        usuario.setRol("rol");
     }
     
     @After
     public void tearDown() {
-    }
-
-
-    /**
-     * Test of getRol method, of class Usuario.
-     */
-    @Test
-    public void testGetRol() {
-        System.out.println("getRol");
-        String expResult = "rol";
-        String result = usuario.getRol();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of setRol method, of class Usuario.
-     */
-    @Test
-    public void testSetRol() {
-        System.out.println("setRol");
-        String rol = "";
-        usuario.setRol(rol);
-        assertNotNull(usuario.getRol());
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getCorreo method, of class Usuario.
-     */
-    @Test
-    public void testGetCorreo() {
-        System.out.println("getCorreo");
-        String expResult = "correo";
-        String result = usuario.getCorreo();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of setCorreo method, of class Usuario.
-     */
-    @Test
-    public void testSetCorreo() {
-        System.out.println("setCorreo");
-        String correo = "";
-        usuario.setCorreo(correo);
-        assertNotNull(usuario.getCorreo());
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getPassword method, of class Usuario.
-     */
-    @Test
-    public void testGetPassword() {
-        System.out.println("getPassword");
-        String expResult = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
-        String result = usuario.getPassword();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        
-    }
-
-    /**
-     * Test of setPassword method, of class Usuario.
-     */
-    @Test
-    public void testSetPassword() {
-        System.out.println("setPassword");
-        String password = "";
-        usuario.setPassword(password);
-        assertNotNull(usuario.getPassword());
-        // TODO review the generated test code and remove the default call to fail.
     }
 
     /**
@@ -128,92 +49,6 @@ public class UsuarioTest {
         String new_password = "1";
         boolean expResult = true;
         boolean result = usuario.cambiarPassword(old_password, new_password);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getNombre method, of class Usuario.
-     */
-    @Test
-    public void testGetNombre() {
-        System.out.println("getNombre");
-        String expResult = "nombre";
-        String result = usuario.getNombre();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of setNombre method, of class Usuario.
-     */
-    @Test
-    public void testSetNombre() {
-        System.out.println("setNombre");
-        String nombre = "nombre2";
-        usuario.setNombre(nombre);
-        assertNotNull(usuario.getNombre());
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of getId method, of class Usuario.
-     */
-    @Test
-    public void testGetId() {
-        System.out.println("getId");
-        Long expResult = Long.MIN_VALUE;
-        Long result = usuario.getId();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of setId method, of class Usuario.
-     */
-    @Test
-    public void testSetId() {
-        System.out.println("setId");
-        Long id = Long.MAX_VALUE;
-        usuario.setId(id);
-        assertNotNull(usuario.getId());
-        
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of hashCode method, of class Usuario.
-     */
-    @Test
-    public void testHashCode() {
-        System.out.println("hashCode");
-        int expResult = -2147483648;
-        int result = usuario.hashCode();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of equals method, of class Usuario.
-     */
-    @Test
-    public void testEquals() {
-        System.out.println("equals");
-        Object object = null;
-        boolean expResult = false;
-        boolean result = usuario.equals(object);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-    }
-
-    /**
-     * Test of toString method, of class Usuario.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        String expResult = "entities.Usuario[ id=-9223372036854775808 ]";
-        String result = usuario.toString();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
     }
